bf88e2b9abc3e4895188ef225f9af7c1b2ae5589
Die <dlibonati@leafnoise.io>
2026-03-11 10:59:48 -0300

    fix: better constants

--- cuttube-api/src/constants/messages.py
+++ cuttube-api/src/constants/messages.py
@@ -1,10 +1,21 @@
+# ##### SUCCESS #####
 MESSAGE_SUCCESS_CUT_VIDEO = "Video cutted."
 MESSAGE_SUCCESS_DELETE_CLIP = "Clip deleted."
 
-MESSAGE_ERROR_API = "Internal server error."
+# ##### ERROR #####
+MESSAGE_ERROR_INTERNAL_SERVER = "Internal server error."
 MESSAGE_ERROR_PYDANTIC = "Pydantic error."
 MESSAGE_ERROR_GENERIC = "Error: {e}"
 MESSAGE_ERROR_VIDEO_TUBE_SERVICE = "VideoTubeService error."
 
-MESSAGE_NOT_FOUND_PATH = "Not found path."
+# ##### NOT #####
+
+# ##### NOT_VALID #####
 MESSAGE_NOT_VALID_FIELDS = "Not valid fields."
+
+# ##### NOT_EXISTS #####
+
+# ##### ALREADY_EXISTS #####
+
+# ##### NOT_FOUND #####
+MESSAGE_NOT_FOUND_PATH = "Not found path."
